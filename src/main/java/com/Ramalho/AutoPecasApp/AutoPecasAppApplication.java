49b639d374fa02c21fd9dce220433b9e8fae4f9b
YuriRamalhoSilva <yuriramalhosilva67@gmail.com>
2025-02-12 14:43:12 -0300

    Reformulando projeto

--- src/main/java/com/Ramalho/AutoPecasApp/AutoPecasAppApplication.java
+++ src/main/java/com/Ramalho/AutoPecasApp/AutoPecasAppApplication.java
@@ -1,13 +1,13 @@
-package empresa.com.ProjetoAutoPecas;
+package com.Ramalho.AutoPecasApp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ProjetoAutoPecasApplication {
+public class AutoPecasAppApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ProjetoAutoPecasApplication.class, args);
+		SpringApplication.run(AutoPecasAppApplication.class, args);
 	}
 
 }
--- src/test/java/com/Ramalho/AutoPecasApp/AutoPecasAppApplicationTests.java
+++ src/test/java/com/Ramalho/AutoPecasApp/AutoPecasAppApplicationTests.java
@@ -1,10 +1,10 @@
-package empresa.com.ProjetoAutoPecas;
+package com.Ramalho.AutoPecasApp;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ProjetoAutoPecasApplicationTests {
+class AutoPecasAppApplicationTests {
 
 	@Test
 	void contextLoads() {
